248ff66cb16fdaef6eefab33994a8b7242301d49
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-04 13:47:25 +0100

    feat(database): add second table

--- config/postgress.js
+++ config/postgress.js
@@ -6,13 +6,19 @@ const pg = require('knex')({
 
 pg.schema.dropTableIfExists('users');
 
-pg.schema.createTable('users', (table) => {
-  table.increments('id').primary();
-  table.string('name').notNullable();
-  table.string('mail').notNullable();
-  table.double('payed').notNullable().defaultTo(0.00);
-  table.double('consumed').notNullable().defaultTo(0.00);
-  table.timestamps();
+pg.schema.createTable('users', (tbl) => {
+  tbl.increments('id').primary();
+  tbl.string('name').notNullable();
+  tbl.string('mail').notNullable();
+  tbl.double('payed').notNullable().defaultTo(0.00);
+  tbl.double('consumed').notNullable().defaultTo(0.00);
+  tbl.timestamps();
 });
+
+pg.schema.createTable('organisations', (tbl) => {
+  tbl.increments('id').primary();
+  tbl.string('name').notNullable();
+  tbl.double('rate').notNullable().defaultTo(0.50);
+})
 
 module.exports = pg;
