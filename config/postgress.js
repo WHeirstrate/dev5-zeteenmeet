202d7fb777395b9bfd2d8eb69cb3f885f0810195
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-11 12:52:18 +0100

    feat(db): replace migration to own file

--- config/postgress.js
+++ config/postgress.js
@@ -4,30 +4,4 @@ const pg = require('knex')({
   searchPath: ['knex', 'public'],
 });
 
-pg.schema.dropTableIfExists('users');
-
-async () => {
-  try {
-    await db.schema.dropTableIfExists('users');
-    await db.schema.withSchema('public').createTable('users', (tbl) => {
-      tbl.increments('id').primary();
-      tbl.string('name').notNullable();
-      tbl.string('mail').notNullable();
-      tbl.double('payed').notNullable().defaultTo(0.00);
-      tbl.double('consumed').notNullable().defaultTo(0.00);
-      tbl.timestamps();
-    });
-    console.log('Created users table!');
-    await db.schema.withSchema('public').createTable('organisations', (tbl) => {
-      tbl.increments('id').primary();
-      tbl.string('name').notNullable();
-      tbl.double('rate').notNullable().defaultTo(0.50);
-    });
-    process.exit(0);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-}
-
 module.exports = pg;
