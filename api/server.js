744eddbcc8e03df0b9a3f6311ee95b958054f477
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-11 23:10:47 +0100

    feat(api): correct routes using docs and postman

--- api/server.js
+++ api/server.js
@@ -26,23 +26,17 @@ app.get('/', (req, res) => {
   res.send('Welcome, RESTful api is running');
 })
 
+// -----------------
+// ----- USERS -----
+// -----------------
+
 /**
  * Fetch all users from the 'users' table.
+ * @returns {Object} - A JSON-object with all users will be returned.
  */
-
-app.get('/users', (res, req) => {
+app.get('/users', (req, res) => {
   pg.select()
     .from('users')
-    .then(data => res.json(data));
-})
-
-/**
- * Fetch all organisations from the 'organisations' table.
- */
-
-app.get('/organisations', (res, req) => {
-  pg.select()
-    .from('organisations')
     .then(data => res.json(data));
 })
 
@@ -53,14 +47,18 @@ app.get('/organisations', (res, req) => {
  * @param mail - The mail of the user that wants to be logged in.
  * @param password - The password that will be compared with the one fetched
  * (and later descrypted) from the database.
+ * @returns {StatusCode} - 200 when user is correctly logged in, 401 when the
+ * wrong credentials were used and 400 when the request doesn't contain at least
+ * an email and a password.
  */
-
 app.post('/users/login', (req, res) => {
-  if (req.body.email && req.body.password) {
+  if (req.body.mail && req.body.password) {
     try {
-      const pass = pg.where({mail: req.body.mail}).select('password');
+      const pass = pg.where({mail: req.body.mail}).returning('password');
       if (pass == req.body.password) {
         res.sendStatus(200);
+      } else {
+        throw new Error('Wrong credentials!');
       }
     } catch(err) {
       console.log(err)
@@ -68,6 +66,54 @@ app.post('/users/login', (req, res) => {
     }
   } else {
     res.sendStatus(400);
+  }
+});
+
+/**
+ * Update a user by id (passed in the uri) with the parameters that get passed
+ * in the body. 
+ * @param payed - The amount the user has spent in total.
+ * @param consumed - The amount the user has consumed in total.
+ * @returns {Object} - The user that was updated. If the user was unable to be 
+ * updated, a status 401 is returned. if the wrong params were passed, a status
+ * 400 will be returned.
+ */
+app.put('/users/:id', (req, res) => {
+  if(req.body.payed && req.body.consumed) {
+    try {
+      pg('users')
+        .where('id', req.params.id)
+        .update({
+          payed: req.body.payed,
+          consumed: req.body.consumed
+        })
+        .returning()
+        .then(data => res.json(data));
+    } catch(err) {
+      console.log(err)
+      res.sendStatus(401);
+    }
+  } else {
+    res.sendStatus(400).json(req.body);
+  }
+});
+
+/**
+ * Delete a user with the specified id (in the uri).
+ * @param id - must be added in the request uri
+ * @returns {StatusCode} - 200 when the user has succesfully been removed, 400 when
+ * the user could not be deleted.
+ */
+
+app.delete('/users/:id', (req, res) => {
+  try {
+    pg('users')
+      .where('id', req.params.id)
+      .del()
+      .then(()=> res.sendStatus(200));
+  } catch(err) {
+    console.log(err);
+    res.send(400);
   }
 });
 
@@ -84,24 +130,39 @@ app.post('/users/login', (req, res) => {
  * combining all it's organisations.
  * @param consumed - (optional) The amount (in euro's) the user has consumed in
  * total, combining all it's organisations.
+ * @returns {Object} - The user that has been added. If this was unsuccesful, a 
+ * statuscode 401 will be returned.
  */
-
 app.post('/users/add', async (req, res) => {
   console.log('\n', req.body, '\n');
   if (req.body.name && req.body.mail && req.body.password) {
     pg('users')
-      .insert({
-        name: req.body.name,
-        mail: req.body.mail,
-        password: req.body.password
-      })
-      .returning('*')
-      .then(user => {
-        res.json(user);
-      });
+    .insert({
+      name: req.body.name,
+      mail: req.body.mail,
+      password: req.body.password
+    })
+    .returning('*')
+    .then(user => {
+      res.json(user);
+    });
   } else {
     return res.sendStatus(401);
   }
+})
+
+// -----------------
+// - ORGANISATIONS -
+// -----------------
+
+/**
+ * Fetch all organisations from the 'organisations' table.
+ * @returns {Object} - A JSON-object with all organisations will be returned.
+ */
+app.get('/organisations', (req, res) => {
+  pg.select()
+    .from('organisations')
+    .then(data => res.json(data));
 })
 
 app.listen(process.env.PORT, () => {
