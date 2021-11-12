9a3fc2a12e45b1da7a1104ae7725db8d63cc6b12
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-12 15:04:47 +0100

    feat(api): split server and index, use dotenv package

---
+++ api/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./server');
+const PORT = process.env.PORT || 5000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`server listening at: http://localhost:${PORT}`);
+});
