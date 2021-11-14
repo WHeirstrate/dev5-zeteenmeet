0e70bdb6b6e42fdf2bf3b3ce9eaa2f92db4c7a9c
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-14 20:26:44 +0100

    feat(db): migrate and seed instantly

--- api/config/postgress.js
+++ api/config/postgress.js
@@ -1,7 +1,14 @@
+const migrate = require('./migrate')
+const seed = require('./seed');
 const pg = require('knex')({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://test:test@localhost:5432/database',
   searchPath: ['knex', 'public'],
 });
+
+(async () => {
+  await migrate.createDbTables(pg);
+  await seed.seedDatabase(pg);
+})();
 
 module.exports = pg;
