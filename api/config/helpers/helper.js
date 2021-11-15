abcb40c1cb1c543ccef3f23e65a8202a2fed6b68
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-15 23:42:48 +0100

    feat(test): create and test helpers

---
+++ api/config/helpers/helper.js
@@ -0,0 +1,31 @@
+function checkAddUserBody(body) {
+  if (!body || !body.name || !body.email || !body.password)
+    return false
+  if (!checkName(body.name) || !checkEmail(body.email) || !checkPassword(body.password))
+    return false;
+  return true;
+}
+
+function checkName(name) {
+  if (!name || !name.length)
+    return false;
+  return true;
+}
+
+function checkEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function checkPassword(pass) {
+  if (!pass || !pass.length)
+    return false;
+  return true
+}
+
+module.exports = {
+  checkAddUserBody,
+  checkName,
+  checkEmail,
+  checkPassword
+};
